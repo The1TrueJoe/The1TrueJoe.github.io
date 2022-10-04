2860f589e24ed677538bd50a5386ad433c63a6fe
Joseph Telaak <47280152+The1TrueJoe@users.noreply.github.com>
2022-10-04 17:11:19 -0400

    Add info

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,57 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
-  `,
+  name: "Joseph Telaak",
+  jobDescription: "Embedded Systems Engineer",
+  about: `I'm an embedded systems engineering and systems architect. I am also a computer engineering major at the University of South Carolina.
+  I enjoy designing robotics control systems, IoT gadgets, and building software systems. I especially enjoy combing all three into projects whenever I can, just like in my autonomous golf cart fleet project.`,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
+      name: "UofSC Cyberinfrastructure Lab - Research Intern",
+      location: "Columbia, SC",
+      startDate: "June 2021",
+      endDate: "July 2021",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
-      ],
-    },
-
-    {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
-      description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
-    },
-
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "- Researched applications of P4 switches.",
+        "- Developed a suite of automated tests for throughput and packet loss measurements.",
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
+      name: "University of South Carolina",
+      location: "Columbia, SC",
+      startDate: "2022",
+      endDate: "2024",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "B.S.E in Computer Engineering", "Minor in Mathematics and Physics",
       ],
     },
     {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      name: "South Carolina Governors School for Science and Mathematics",
+      location: "Hartsville, SC",
+      startDate: "2020",
+      endDate: "2022",
+      description: [
+        "High School Diploma", "Concentration in Computer Science and Mathematics"
+      ],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    github: "https://github.com/The1TrueJoe",
+    email: "jttelaak04@gmail.com",
+    linkedin: "https://www.linkedin.com/in/jtelaak/",
   },
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: "Self-Driving Golf Cart",
       isFeatured: true,
       thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+      githubUrl: "https://github.com/autogolfcart",
+      liveUrl: "https://autogolfc.art",
     },
   ],
 };
